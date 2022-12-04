0a0b039f2c630838d1770894f3a3910969e7af77
PriyanshGarg15 <116974262+PriyanshGarg15@users.noreply.github.com>
2022-12-04 17:17:27 +0530

    Create 9.greatest.c

---
+++ 9.greatest.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    int n,p,i;
+    printf("enter size of array u need");
+    scanf("%d",&n);
+    int a[n];
+    printf("enter elements of array");
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(int i=0;i<n;i++)
+    {
+        printf("%d\t",a[i]);
+    }
+    p=a[0];
+    for(i=1;i<n;i++)
+    {
+        if(a[i]>p)
+        {
+            p=a[i];
+        }
+    }
+    printf("\nmax is=%d",p);
+}
+
+
